6730e8ddba932ccec1f871c0ff12243651c2f69a
andrewimwilliams <andrewimwilliams@gmail.com>
2026-02-16 17:36:32 -0500

    Implement Port 5 ISR for S1 to toggle LED blink state

--- src/main.c
+++ src/main.c
@@ -1,20 +1,59 @@
 #include <msp430.h>
 
-static void delay(void) {
-    volatile unsigned long i;
-    for (i = 0; i < 60000; i++) { __no_operation(); }
+volatile unsigned char blinking = 0;
+
+static void delay_ms(unsigned int ms)
+{
+    while (ms--)
+    {
+        __delay_cycles(1000);  // assumes ~1 MHz
+    }
 }
 
-int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;      // stop watchdog
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // Stop watchdog
+    PM5CTL0 &= ~LOCKLPM5;       // Unlock GPIO
 
-    PM5CTL0 &= ~LOCKLPM5;          // <<< IMPORTANT on FRAM devices
-
-    P1DIR |= BIT0;                 // LED1 (P1.0)
+    // LED: P1.0
+    P1DIR |= BIT0;
     P1OUT &= ~BIT0;
 
-    while (1) {
-        P1OUT ^= BIT0;
-        delay();
+    // Button S1: P5.6
+    P5DIR &= ~BIT6;             // input
+    P5REN |= BIT6;              // enable resistor
+    P5OUT |= BIT6;              // pull-up
+    P5IES |= BIT6;              // falling edge (press)
+    P5IFG &= ~BIT6;             // clear flag
+    P5IE  |= BIT6;              // enable interrupt
+
+    __enable_interrupt();
+
+    while (1)
+    {
+        if (blinking)
+        {
+            P1OUT ^= BIT0;
+            delay_ms(200);
+        }
+        else
+        {
+            P1OUT &= ~BIT0;     // keep LED off
+            __no_operation();   // optional: place breakpoint / keep loop non-empty
+        }
+    }
+}
+
+// GCC-style ISR for Port 5
+void __attribute__((interrupt(PORT5_VECTOR))) Port_5_ISR(void)
+{
+    if (P5IFG & BIT6)
+    {
+        blinking ^= 1;          // toggle state
+
+        // crude debounce (fine for now)
+        delay_ms(20);
+
+        P5IFG &= ~BIT6;         // clear flag
     }
 }
